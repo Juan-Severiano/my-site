8225a71dd634a6ec6164b40da2588644d4e2decc
juan <juansoussev@gmail.com>
2022-12-10 13:39:37 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def my_view(request):
     return HttpResponse('sobre')
 
+
 def home(request):
     return HttpResponse('Home')
+
 
 def contato(request):
     return HttpResponse('contato')
