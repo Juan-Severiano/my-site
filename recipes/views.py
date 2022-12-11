f2d8c885c2db045a20a1f13c0737c712a7acd159
juan <juansoussev@gmail.com>
2022-12-11 16:12:22 -0300

    Trabalho de FrontEnd iniciado

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def sobre(request):
-    return render(request, 'home.html')
-
-
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Francisco Juan',
+    })
